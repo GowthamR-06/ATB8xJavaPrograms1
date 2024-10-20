370e1ff7fe3622e13a81ecfcaa82b3223d572f43
Gowtham <gowtham96241@gmail.com>
2024-10-20 18:50:42 +0530

    Today task

---
+++ src/sept/ex_05092024/Lab004.java
@@ -0,0 +1,7 @@
+package sept.ex_05092024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/sept/ex_05092024/Lab006.java
@@ -0,0 +1,9 @@
+package sept.ex_05092024;
+
+public class Lab006 {
+    //Lab exercise single line comments
+
+    /*
+    this is  used for multi line commands
+     */
+}
